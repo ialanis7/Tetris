974756559ac378c1c6d993128cc2ea463b8812c8
Ignacio Alanis Jr <ignacio.alanis01@utrgv.edu>
2022-02-01 23:34:37 -0600

    Created Game state class

---
+++ Tetris/GameState.cs
@@ -0,0 +1,21 @@
+﻿namespace Tetris
+{
+	public class GameState
+	{
+		private Block currentBlock;
+
+		public Block CurrentBlock
+		{
+			get => currentBlock;
+			private set
+			{
+				currentBlock = value;
+				currentBlock.Reset();
+			}
+		}
+
+		public GameGrid GameGrid { get; }
+		public BlockQueue BlockQueue{get;}
+		public bool GameOver { get; private set; }
+	}
+}

ed1b4c4f9db0983b05d0c7deb17fe94069e9e24c
Ignacio Alanis Jr <ignacio.alanis01@utrgv.edu>
2022-02-01 20:51:18 -0600

    Added Block class

--- Tetris/Block.cs
+++ Tetris/Block.cs
@@ -28,5 +28,36 @@ namespace Tetris
 
 		}
 
+		public void RotateCW()
+		{
+			rotationState = (rotationState + 1) % Tiles.Length;
+		}
+
+		public void RotateCCW()
+		{
+			if (rotationState == 0)
+			{
+				rotationState = Tiles.Length - 1;
+			}
+			else
+			{
+				rotationState--;
+			}
+		}
+
+		public void Move(int rows, int columns)
+		{
+			offset.Row += rows;
+			offset.Column += columns;
+		}
+
+		public void Reset()
+		{
+			rotationState = 0;
+			offset.Row = StartOffset.Row;
+			offset.Column = StartOffset.Column;
+		}
+
+
 	}
 }
